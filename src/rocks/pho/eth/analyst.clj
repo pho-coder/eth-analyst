be9c3e87a196828396ffbd07652d7bdaad8a7d82
Phoenix Zhang <pho.coderzhang@gmail.com>
2017-07-16 20:01:21 +0800

    fix

--- src/rocks/pho/eth/analyst.clj
+++ src/rocks/pho/eth/analyst.clj
@@ -8,7 +8,61 @@
             [rocks.pho.eth.analyst.utils :as utils]
             [rocks.pho.eth.analyst.analysis :as analysis]))
 
+(def cli-options
+  [["-f" "--file file" "raw file"]
+   ["-h" "--help"]])
+
+(defn usage [options-summary]
+  (->> ["This is my program. There are many like it, but this one is mine."
+        ""
+        "Usage: program-name [options] action"
+        ""
+        "Options:"
+        options-summary
+        ""
+        "Actions:"
+        "  one      Start one analyst"
+        ""
+        "Please refer to the manual page for more information."]
+       (clojure.string/join \newline)))
+
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (clojure.string/join \newline errors)))
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
+
+(defn stop-app []
+  (doseq [component (:stopped (mount/stop))]
+    (log/info component "stopped"))
+  (shutdown-agents))
+
+(defn run-one
+  []
+  (log/info "run one"))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    ;; Handle help and error conditions
+    (log/info options)
+    (cond
+      (:help options) (exit 0 (usage summary))
+      (not= (count arguments) 1) (exit 1 (usage summary))
+      errors (exit 1 (error-msg errors)))
+    
+    (.addShutdownHook (Runtime/getRuntime) (Thread. stop-app))
+    (doseq [component (-> args
+                          mount/start-with-args
+                          :started)]
+      (log/info component "started"))
+    
+    ;; Execute program with options
+    (let [raw-file (:raw-file options)]
+      (log/info "raw file:" raw-file)
+      (case (first arguments)
+        "one" (run-one)
+        (exit 1 (usage summary))))))
