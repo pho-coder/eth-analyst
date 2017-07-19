f2ea0812794bd02d36c1127bf111e97dfb410168
Phoenix Zhang <pho.coderzhang@gmail.com>
2017-07-20 00:30:41 +0800

    fix

--- src/rocks/pho/eth/analyst/analysis.clj
+++ src/rocks/pho/eth/analyst/analysis.clj
@@ -8,14 +8,12 @@
 (mount/defstate raw-data-queue :start (java.util.concurrent.LinkedBlockingQueue. 10000))
 
 (defn analysis-some [start-time end-time]
-  (loop [re ""
-         one (.poll raw-data-queue 10 (java.util.concurrent.TimeUnit/SECONDS))]
+  (loop [one (.poll raw-data-queue 10 (java.util.concurrent.TimeUnit/SECONDS))]
     (if (nil? one)
-      re
+      (log/info "NO Got! finish!")
       (let [one (json/read-str one
                                :key-fn keyword)]
         (log/info (:ch one))
-        (recur re
-               (.poll raw-data-queue 60 (java.util.concurrent.TimeUnit/SECONDS)))))))
+        (recur (.poll raw-data-queue 10 (java.util.concurrent.TimeUnit/SECONDS)))))))
 
 (mount/defstate analysis-task :start (future (analysis-some 1 2)))
